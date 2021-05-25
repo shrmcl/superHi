6b0b471856a9f357bd256f127365036253daf591
Shar McLeod <shr.mcl@gmail.com>
2021-05-25 14:15:31 -0500

    completed code for all Page Transition projecs

--- pageTransitions/project3_wanderers/scripts.js
+++ pageTransitions/project3_wanderers/scripts.js
@@ -13,7 +13,11 @@ wiperText.innerHTML = "Wanderers"
 wiperHolder.appendChild(wiperText)
 
 wiper.appendChild(wiperImage)
+wiper.appendChild(wiperHolder)
 bodyTag.appendChild(wiper)
+
+// init the Prefetch scxript to pre-load links in the viewport
+barba.use(barbaPrefetch)
 
 
 // BARBA + GSAP
@@ -22,6 +26,14 @@ barba.init({
   transitions: [
     {
       name: "next",
+      custom({ current, next, trigger }) {
+        // only for our next button
+        // also applying barba's built-in browser "forward" trigger
+        return (trigger.classList && trigger.classList.contains("next")) || trigger === "forward"
+      },
+
+
+
       // stuff that happens when we leave a page
       leave( {current, next, trigger} ) {
         return new Promise(resolve => {
@@ -48,6 +60,10 @@ barba.init({
       },
       // stuff that happens between pages (on "wiper")
       beforeEnter( {current, next, trigger} ) {
+        // set text to the data attribute 'data-title' for next page 'article' container
+        wiperText.innerHTML = next.container.getAttribute("data-title")
+
+
         return new Promise(resolve => {
           const timeline = gsap.timeline({
             defaults: {
@@ -87,6 +103,81 @@ barba.init({
 
         })
       }
+    },
+    {
+      name: "previous",
+      leave( {current, next, trigger} ) {
+        return new Promise(resolve => {
+          // add a gsap timeline 
+          const timeline = gsap.timeline({
+            onComplete() {
+              current.container.remove()
+              resolve()
+            }
+          })
+          // select all header / next+previous tags inside the "container" of "current" page
+          const navigation = current.container.querySelectorAll("header, a.next, a.previous")
+          const photos = current.container.querySelectorAll("div.photos")
+          // use timeline to make these elements go "to" a new opacity
+          timeline
+            .set(wiper, { x: "100%" })
+            .set(wiperImage, { opacity: 0 })
+            .set(wiperText, { y: "100%" })
+            .to(navigation, {opacity:0}, 0)
+            .to(photos, { opacity: 0, x: -500 }, 0)
+            .to(wiper, { x: 0 }, 0)
+
+        })
+      },
+      // stuff that happens between pages (on "wiper")
+      beforeEnter( {current, next, trigger} ) {
+        // set text to the data attribute 'data-title' for next page 'article' container
+        wiperText.innerHTML = next.container.getAttribute("data-title")
+
+
+        return new Promise(resolve => {
+          const timeline = gsap.timeline({
+            defaults: {
+              duration: 1
+            },
+            onComplete() {
+              resolve()
+            }
+          })
+
+          timeline  
+            .to(wiperImage, { opacity: 1 }, 0 )
+            .to(wiperText, { y: 0 }, 0 )
+            .to(wiperText, { y: "100%"}, 2 )
+            .to(wiperImage, { opacity: 0}, 2)
+
+        })
+      },
+      // stuff that happens when we enter new page
+      enter( {current, next, trigger} ) {
+        return new Promise(resolve => {
+          const timeline = gsap.timeline({
+            onComplete() {
+            resolve()
+          }
+        })
+
+        const navigation = next.container.querySelectorAll("header, a.next, a.previous")
+        const photos = next.container.querySelectorAll("div.photos")
+
+        timeline
+          .set(navigation, { opacity: 0 })
+          .set(photos, { opacity: 0, x: 500 })
+          .to(navigation, { opacity: 1 }, 0)
+          .to(photos, { opacity: 1, x: 0 }, 0)
+          .to(wiper, { x: "-100%" }, 0)
+
+        })
+      }
+
+
+
+
     }
   ],
   views: []
