ad1899d25a481741de5bd05503fcf2c2ee55ca35
Shar McLeod <shr.mcl@gmail.com>
2021-07-19 22:59:41 -0500

    added event handling to slideshow & css

--- introReact/01_zesty/src/App.js
+++ introReact/01_zesty/src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
-import cabbage from './assets/image1.jpeg'
-import mango from './assets/image2.jpeg'
-import fig from './assets/image3.jpeg'
-import gaze from './assets/image4.jpeg'
-import peach from './assets/image5.jpeg'
-import avocado from './assets/image6.jpeg'
+import cabbage from './assets/image1.jpeg';
+import mango from './assets/image2.jpeg';
+import fig from './assets/image3.jpeg';
+import gaze from './assets/image4.jpeg';
+import peach from './assets/image5.jpeg';
+import avocado from './assets/image6.jpeg';
 
-const images = [cabbage, mango, fig, gaze, peach, avocado]
+const images = [cabbage, mango, fig, gaze, peach, avocado];
 
 const App = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const handleClick = () => {
+    const length = images.length - 1;
+
+    setCurrentImage((currentImage) => {
+      return currentImage < length ? currentImage + 1 : 0
+    });
+  };
+
   return (
-    <div className="App">
-      <div className="title">
+    <section>
+      <header>
         <h1>Zesty</h1>
         <h2>A photography project <br /> by Ella Fielding</h2>
-      </div>
-      <div className="image-container">
-        <img src={images[0]} alt="" />
-      </div>
-    </div>
+      </header>
+
+      <figure>
+        <figcaption>{currentImage + 1} / {images.length}</figcaption>
+        <img src={images[currentImage]} alt="" onClick={handleClick} />
+      </figure>
+    </section>
   )
 }
 
